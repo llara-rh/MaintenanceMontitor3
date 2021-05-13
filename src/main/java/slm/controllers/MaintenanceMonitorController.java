3cec03ce8e6e92cf0d225bafd8a5bb4d4b970dac
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2021-05-13 20:24:55 +0200

    Infrastructure set up and controller prepared

---
+++ src/main/java/slm/controllers/MaintenanceMonitorController.java
@@ -0,0 +1,16 @@
+package slm.controllers;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("api/MaintenanceMonitor3/")
+public class MaintenanceMonitorController {
+    @GET
+    @Path("/{sourceString}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMaintenanceStringFromSourceString(@PathParam("sourceString") String sourceString){
+        return sourceString;
+    }
+}
